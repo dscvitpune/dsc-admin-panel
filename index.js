316c8b8aa190e96bdac894f0399302a96c32e23a
kulkarni-anurag <kanurag0801@gmail.com>
2021-10-10 21:42:27 +0530

    Passport Middleware Added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 require('dotenv').config();
 
@@ -10,6 +13,28 @@ const PORT = 5000 || process.env.PORT;
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+  
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 const uri = process.env.ATLAS_URI;
 mongoose.Promise = global.Promise;
