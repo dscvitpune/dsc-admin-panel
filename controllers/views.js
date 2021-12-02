74d4db64c2819fd853cd037af2d22970a1da7925
Vedant Gokhale <gokhalevedant06@gmail.com>
2021-12-02 14:26:38 +0530

    Fixed static files errors

--- controllers/views.js
+++ controllers/views.js
@@ -1,8 +1,23 @@
-
 const home = (req, res) => {
-    res.render('pages/index');
-}
+  res.render("pages/index");
+};
+const teams = (req, res) => {
+  res.render("pages/teams");
+};
+const events = (req, res) => {
+  res.render("pages/events");
+};
+const signIn = (req, res) => {
+  res.render("pages/signin");
+};
+const projects = (req, res) => {
+  res.render("pages/projects");
+};
 
 module.exports = {
-    home
-}
+  home,
+  teams,
+  events,
+  signIn,
+  projects
+};
