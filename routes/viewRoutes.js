91e9fe6f2d690f9d6075c0dd077c47040a6c51ca
Vedant Gokhale <gokhalevedant06@gmail.com>
2021-12-04 21:50:00 +0530

    Frontend auth done,logout functionality added, image errors solved

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,10 @@
 const { home, teams, events,signIn,projects } = require("../controllers/views");
-
+const {restrictUnauth,forwardAuth} = require("../middlewares/requireAuth")
 module.exports = (app) => {
-  app.get("/", home);
-  app.get("/teams", teams);
-  app.get("/event", events);
+  app.get("/",restrictUnauth, home);
+  app.get("/teams",restrictUnauth, teams);
+  app.get("/event",restrictUnauth,events);
   app.get("/signin", signIn);
-  app.get("/project", projects);
+  app.get("/project",restrictUnauth, projects);
 };
 
